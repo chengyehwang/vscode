f74d542baa8bde470e52205b9a3fda8ce3835a26
chengyehwang <cywang.eda@gmail.com>
2020-12-29 21:59:34 +0800

    add python

--- install_ext.sh
+++ install_ext.sh
@@ -1,2 +1,2 @@
 wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-toolsai/vsextensions/jupyter/2020.12.414227025/vspackage -O jupyter.vsix
-
+wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/python/2020.12.424452561/vspackage -O python.vsix
