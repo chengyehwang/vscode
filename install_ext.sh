b273e7909820ed7def19236381a30e9bcef107d3
chengyehwang <cywang.eda@gmail.com>
2020-12-30 07:12:13 +0800

    fix

--- install_ext.sh
+++ install_ext.sh
@@ -1,10 +1,10 @@
 publisher="ms-python"
 extension_name="python"
-version="2020.12.424452561"
+version="2020.5.86806"
 
 wget "https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${extension_name}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
 mv Microsoft.VisualStudio.Services.VSIXPackage /opt/${publisher}.vsix
-code-server --install-extension ${publisher}.vsix --extensions-dir /vscode/ext
+#code-server --install-extension ${publisher}.vsix --extensions-dir /vscode/ext
 
 publisher="ms-toolsai"
 extension_name="jupyter"
@@ -12,6 +12,6 @@ version="2020.12.1"
 
 wget "https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${extension_name}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
 mv Microsoft.VisualStudio.Services.VSIXPackage /opt/${publisher}.vsix
-code-server --install-extension ${publisher}.vsix --extensions-dir /vscode/ext
+#code-server --install-extension ${publisher}.vsix --extensions-dir /vscode/ext
 
 echo "end"
