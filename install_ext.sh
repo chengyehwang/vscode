516d192f4eb3ebafc84d4365410c50b1a9912c9b
chengyehwang <cywang.eda@gmail.com>
2021-01-01 21:16:56 +0800

    for loop to get install

--- install_ext.sh
+++ install_ext.sh
@@ -1,17 +1,22 @@
-publisher="ms-python"
-extension_name="python"
-version="2020.11.371526539"
-
+for index in {1..100};
+do
+if [ $index == 1 ]
+then
+    publisher="ms-python"
+    extension_name="python"
+    version="2020.11.371526539"
+elif [ $index == 2 ]
+then
+    publisher="ms-toolsai"
+    extension_name="jupyter"
+    version="2020.11.399280825"
+else
+    exit 0
+fi
 wget "https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${extension_name}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
 mv Microsoft.VisualStudio.Services.VSIXPackage /opt/${publisher}.vsix
 #code-server --install-extension ${publisher}.vsix --extensions-dir /vscode/ext
 
-publisher="ms-toolsai"
-extension_name="jupyter"
-version="2020.11.399280825"
-
-wget "https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${extension_name}/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
-mv Microsoft.VisualStudio.Services.VSIXPackage /opt/${publisher}.vsix
-#code-server --install-extension ${publisher}.vsix --extensions-dir /vscode/ext
+done
 
 echo "end"
