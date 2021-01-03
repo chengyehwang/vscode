657a8028e402fc2c4279be93e6540781544981e3
chengyehwang <cywang.eda@gmail.com>
2021-01-03 08:10:08 +0800

    fix

--- start_vscode.sh
+++ start_vscode.sh
@@ -29,4 +29,5 @@ echo "*******************************************"
 
 #export NODE_OPTIONS=--max-old-space-size=4096
 #jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
-code-server --auth none --bind-addr $host:8888 --cert /vscode/${host_ext}.pem --cert-key /vscode/${host_ext}-key.pem --extensions-dir /opt/ext
+#code-server --auth none --bind-addr $host:8888 --cert /vscode/${host_ext}.pem --cert-key /vscode/${host_ext}-key.pem --extensions-dir /opt/ext
+code-server --auth none --bind-addr $host:8888 --cert /opt/ca/rootCA.pem --cert-key /opt/ca/rootCA-key.pem --extensions-dir /opt/ext
